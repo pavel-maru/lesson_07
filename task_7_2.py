91ef320460c6f1f69dc016aadc248a55166c076d
pavel-maru <71653912+pavel-maru@users.noreply.github.com>
2021-02-12 00:08:55 +0300

    first commit

--- task_7_2.py
+++ task_7_2.py
@@ -5,41 +5,40 @@
 
 from random import uniform
 
-def merge_sort(lst):
+def merge_sort(array):
 
-    if len(lst) <= 1:
-        return lst
+    if len(array) <= 1:
+        return array
 
-    sep = len(lst) // 2
-    left = merge_sort(lst[:sep])
-    right = merge_sort(lst[sep:])
+    sep = len(array) // 2
+    left = merge_sort(array[:sep])
+    right = merge_sort(array[sep:])
     # print(left, right)
 
     i = j = 0
-    res_lst = []
+    res_arr = []
 
     while i < len(left) or j < len(right):
         if i >= len(left):
-            res_lst.append(right[j])
+            res_arr.append(right[j])
             j += 1
         elif j >= len(right):
-            res_lst.append(left[i])
+            res_arr.append(left[i])
             i += 1
         elif left[i] < right[j]:
-            res_lst.append(left[i])
+            res_arr.append(left[i])
             i += 1
         else:
-            res_lst.append(right[j])
+            res_arr.append(right[j])
             j += 1
         # print(res)
 
-    return res_lst
+    return res_arr
 
 SIZE = 20
 MIN_ITEM = 0
 MAX_ITEM = 50
-lst = [round(uniform(MIN_ITEM, MAX_ITEM), 1) for _ in range(SIZE)]
+array = [round(uniform(MIN_ITEM, MAX_ITEM), 1) for _ in range(SIZE)]
 
-print(lst)
-print(merge_sort(lst))
-# print(lst)
+print(array)
+print(merge_sort(array))
